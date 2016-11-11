e732d333a4532c1179ad93949f74a9939833af70
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-11-11 22:18:30 +0530

    Creates Java Files for creating tables in database datacrate student data files

--- src/DatabaseCreateTables.java
+++ src/DatabaseCreateTables.java
@@ -21,10 +21,10 @@ public class DatabaseCreateTables {
                     "dep_name VARCHAR(255)," +
                     "PRIMARY KEY(dep_id))");
 
-            statement.executeUpdate("CREATE TABLE student_details(" +
+            statement.executeUpdate("CREATE TABLE s_details(" +
                     "s_roll INTEGER NOT NULL," +
-                    "last_name VARCHAR(255)," +
                     "first_name VARCHAR(255) NOT NULL," +
+                    "last_name VARCHAR(255)," +
                     "year INTEGER NOT NULL," +
                     "dep_id VARCHAR(10) NOT NULL," +
                     "address VARCHAR(255)," +
@@ -32,10 +32,10 @@ public class DatabaseCreateTables {
                     "FOREIGN KEY(year) REFERENCES years(year)," +
                     "FOREIGN KEY(dep_id) REFERENCES departments(dep_id))");
 
-            statement.executeUpdate("CREATE TABLE employee_details(" +
+            statement.executeUpdate("CREATE TABLE e_details(" +
                     "e_roll INTEGER NOT NULL," +
-                    "last_name VARCHAR(255)," +
                     "first_name VARCHAR(255) NOT NULL," +
+                    "last_name VARCHAR(255)," +
                     "dep_id VARCHAR(10) NOT NULL," +
                     "address VARCHAR(255)," +
                     "PRIMARY KEY(e_roll)," +
@@ -45,19 +45,55 @@ public class DatabaseCreateTables {
                     "c_id VARCHAR(10) NOT NULL," +
                     "c_name VARCHAR(255)," +
                     "year INTEGER NOT NULL," +
-                    "dep_id VARCHAR(10) NOT NULL," +
                     "PRIMARY KEY(c_id)," +
-                    "FOREIGN KEY(year) REFERENCES years(year)," +
-                    "FOREIGN KEY(dep_id) REFERENCES departments(dep_id))");
+                    "FOREIGN KEY(year) REFERENCES years(year))");
 
             statement.executeUpdate("CREATE TABLE books(" +
                     "b_id VARCHAR(10) NOT NULL," +
                     "b_name VARCHAR(255)," +
                     "PRIMARY KEY(b_id))");
 
+            statement.executeUpdate("CREATE TABLE s_courses(" +
+                    "roll INTEGER NOT NULL," +
+                    "c_id VARCHAR(10) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES s_details(s_roll)," +
+                    "FOREIGN KEY(c_id) REFERENCES courses(c_id))");
+
+            statement.executeUpdate("CREATE TABLE e_courses(" +
+                    "roll INTEGER NOT NULL," +
+                    "c_id VARCHAR(10) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES e_details(e_roll)," +
+                    "FOREIGN KEY(c_id) REFERENCES courses(c_id))");
+
+            statement.executeUpdate("CREATE TABLE s_books(" +
+                    "roll INTEGER NOT NULL," +
+                    "b_id VARCHAR(10) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES s_details(s_roll)," +
+                    "FOREIGN KEY(b_id) REFERENCES books(b_id))");
+
+            statement.executeUpdate("CREATE TABLE e_books(" +
+                    "roll INTEGER NOT NULL," +
+                    "b_id VARCHAR(10) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES e_details(e_roll)," +
+                    "FOREIGN KEY(b_id) REFERENCES books(b_id))");
+
+            statement.executeUpdate("CREATE TABLE s_login(" +
+                    "roll INTEGER NOT NULL," +
+                    "pass VARCHAR(255) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES s_details(s_roll))");
+
+            statement.executeUpdate("CREATE TABLE e_login(" +
+                    "roll INTEGER NOT NULL," +
+                    "pass VARCHAR(255) NOT NULL," +
+                    "FOREIGN KEY(roll) REFERENCES e_details(e_roll))");
+
             statement.executeUpdate("CREATE TABLE admin(" +
                     "u_id VARCHAR(255) NOT NULL," +
                     "password VARCHAR(255) NOT NULL)");
+
+            statement.executeUpdate("INSERT INTO admin VALUES('admin','admin123')");//admin's username and password
+
+            statement.executeUpdate("");
 
 
 
