19ea2a2dc5cad4f64369e8331a7968ded49c7ab9
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-11-12 10:43:04 +0530

    More Changes in database entry code and also added passwords to edata files

--- src/DatabaseEntry.java
+++ src/DatabaseEntry.java
@@ -12,28 +12,52 @@ import java.sql.SQLException;
 public class DatabaseEntry {
     public static void main(String[] args) {
         try {
-            String dbString="jdbc:mysql://localhost:3306/datacrate?user=root&password=mysql&useSSL=false";
-            FileReader fr=new FileReader("./src/data/years.txt");
-            BufferedReader br=new BufferedReader(fr);
-            Connection connection= DriverManager.getConnection(dbString);
-            PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO years VALUES(?)");
-            String str="";
+            String dbString = "jdbc:mysql://localhost:3306/datacrate?user=root&password=mysql&useSSL=false";
+            FileReader fr = new FileReader("./src/data/years.txt");
+            BufferedReader br = new BufferedReader(fr);
+            Connection connection = DriverManager.getConnection(dbString);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO years VALUES(?)");
+            String str = "";
             String[] arr;
-            while ((str=br.readLine())!=null){
-                preparedStatement.setInt(1,Integer.valueOf(str));
+            while ((str = br.readLine()) != null) {
+                preparedStatement.setInt(1, Integer.valueOf(str));
                 preparedStatement.executeUpdate();
             }
             fr.close();
             br.close();
             preparedStatement.close();
-            fr=new FileReader("./src/data/courses.txt");
-            br=new BufferedReader(fr);
-            preparedStatement=connection.prepareStatement("INSERT INTO courses VALUES(?,?,?)");
-            while ((str=br.readLine())!=null) {
-                arr=str.split(" ");
-                preparedStatement.setString(1,arr[0]);
-                preparedStatement.setString(2,arr[1]);
-                preparedStatement.setInt(3,Integer.valueOf(arr[2]));
+            fr = new FileReader("./src/data/courses.txt");
+            br = new BufferedReader(fr);
+            preparedStatement = connection.prepareStatement("INSERT INTO courses VALUES(?,?,?)");
+            while ((str = br.readLine()) != null) {
+                arr = str.split(" ");
+                preparedStatement.setString(1, arr[0]);
+                preparedStatement.setString(2, arr[1]);
+                preparedStatement.setInt(3, Integer.valueOf(arr[2]));
+                preparedStatement.executeUpdate();
+            }
+            fr.close();
+            br.close();
+            preparedStatement.close();
+            fr = new FileReader("./src/data/books.txt");
+            br = new BufferedReader(fr);
+            preparedStatement = connection.prepareStatement("INSERT INTO books VALUES(?,?)");
+            while ((str = br.readLine()) != null) {
+                arr = str.split(" ");
+                preparedStatement.setString(1, arr[0]);
+                preparedStatement.setString(2, arr[1]);
+                preparedStatement.executeUpdate();
+            }
+            fr.close();
+            br.close();
+            preparedStatement.close();
+            fr = new FileReader("./src/data/dept.txt");
+            br = new BufferedReader(fr);
+            preparedStatement = connection.prepareStatement("INSERT INTO departments VALUES(?,?)");
+            while ((str = br.readLine()) != null) {
+                arr = str.split(" ");
+                preparedStatement.setString(1, arr[0]);
+                preparedStatement.setString(2, arr[1]);
                 preparedStatement.executeUpdate();
             }
 
